6666eb9f9c9237bfc925d96b7037a17d462f77da
LiZhen <3081085609@qq.com>
2024-10-07 16:35:46 +0800

    feat: 补充 weather.d.ts 的类型注释

--- src/service/types/weather.d.ts
+++ src/service/types/weather.d.ts
@@ -141,19 +141,33 @@ declare namespace Api {
             }
             type Response = BaseInfo & {
                 hourly: {
+                    /** 预报时间 */
                     fxTime: string;
+                    /** 温度，默认单位：摄氏度 */
                     temp: string;
+                    /** 天气状况的图标代码 */
                     icon: string;
+                    /** 天气状况的文字描述，包括阴晴雨雪等天气状态的描述 */
                     text: string;
+                    /** 风向360角度 */
                     wind360: string;
+                    /** 风向 */
                     windDir: string;
+                    /** 风力等级 */
                     windScale: string;
+                    /** 风速，公里/小时 */
                     windSpeed: string;
+                    /** 相对湿度，百分比数值 */
                     humidity: string;
+                    /** 当前小时累计降水量，默认单位：毫米 */
                     pop: string;
+                    /** 逐小时预报降水概率，百分比数值，可能为空 */
                     precip: string;
+                    /** 大气压强，默认单位：百帕 */
                     pressure: string;
+                    /** 云量，百分比数值。可能为空 */
                     cloud: string;
+                    /**  露点温度。可能为空 */
                     dew: string;
                 }[];
             }
@@ -227,15 +241,25 @@ declare namespace Api {
                     name: string;
                     /** station.id 监测站ID */
                     id: string;
+                    /** 空气质量指数 */
                     aqi: string;
+                    /** 空气质量指数等级 */
                     level: string;
+                    /** 空气质量指数级别 */
                     category: string;
+                    /** 空气质量的主要污染物，空气质量为优时，返回值为NA */
                     primary: string;
+                    /** PM10 */
                     pm10: string;
+                    /** PM2.5 */
                     pm2p5: string;
+                    /** 二氧化氮 */
                     no2: string;
+                    /** 二氧化硫 */
                     so2: string;
+                    /** 一氧化碳 */
                     co: string;
+                    /** 臭氧 */
                     o3: string;
                 }[];
             }
